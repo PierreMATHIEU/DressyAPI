2b03045db5ead4f7eb4c37cd7d85bb52363515cb
PierreMATHIEU <p.mathieu742@gmail.com>
2017-04-06 19:48:42 +0200

    database heroku

--- include/Config.php
+++ include/Config.php
@@ -2,10 +2,10 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'dressy');
+define('DB_USERNAME', 'mfpyjwdlehhhvo');
+define('DB_PASSWORD', '008e494e873a65a1b1c70d46ec85074032c08ae7eaa7a53e78ae4c2bb195caa8');
+define('DB_HOST', 'ec2-54-243-124-240.compute-1.amazonaws.com');
+define('DB_NAME', 'dccpro45ju5lof');
 
 define('USER_CREATED_SUCCESSFULLY', 0);
 define('USER_CREATE_FAILED', 1);
