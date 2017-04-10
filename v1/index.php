64b98ea34af5b42c8a1f126b72c1b459f7dae4c3
PierreMATHIEU <p.mathieu742@gmail.com>
2017-04-10 15:36:18 +0200

    Register

--- v1/index.php
+++ v1/index.php
@@ -70,7 +70,7 @@ $app->post('/register', function() use ($app) {
 
             // validating email address
             var_dump($user);
-            var_dump($user->getUser_mail());die;
+            //var_dump($user->getUser_mail());die;
 
             validateEmail($user->getUser_mail());
 
