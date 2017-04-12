070b9aa8dbd1dcbb80673a80ca69a97d9337a03d
PierreMATHIEU <p.mathieu742@gmail.com>
2017-04-12 19:33:06 +0200

    register

--- models/User.php
+++ models/User.php
@@ -8,23 +8,21 @@ class User{
     private $user_mail;
     private $user_country;
 
-    public function __construct($user_pseudo, $user_password, $user_fisrtName, $user_lastName, $user_mail, $user_country) {
+   /* public function __construct($user_pseudo, $user_password, $user_fisrtName, $user_lastName, $user_mail, $user_country) {
       $this->user_pseudo = $user_pseudo;
       $this->user_password = $user_password;
       $this->user_fisrtName = $user_fisrtName;
       $this->user_lastName = $user_lastName;
       $this->user_mail = $user_mail;
       $this->user_country = $user_country;
-     }
+     }*/
 
     /**
      * User constructor.
      * @param $user_pseudo
      * @param $user_password
      */
-    public function __constructLogin ($user_pseudo, $user_password) {
-       $this->user_pseudo = $user_pseudo;
-       $this->user_password = $user_password;
+    public function __construct() {
       }
       
     public function setUser_pseudo($user_pseudo) {
