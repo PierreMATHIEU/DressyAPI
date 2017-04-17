956b809f0345aa57acfa70389da2a35cd8c8547e
PierreMATHIEU <p.mathieu742@gmail.com>
2017-04-17 17:27:21 +0200

    clothing

--- v1/index.php
+++ v1/index.php
@@ -252,7 +252,6 @@ $app->get('/clothing/:clothing_id', 'authenticate', function($clothing_id){
         // fetching all user tasks
         $result = $db->viewClothing($user_id,$clothing_id);
         $res = $db->viewDetailsClothing($user_id,$clothing_id);
-        var_dump($res);
         if($result){
             $response['status'] = "success";
             $response['urlImage'] = $res->getUrlImage();
