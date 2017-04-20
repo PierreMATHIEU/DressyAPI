639db09a85306d838a8e15104ff2ce42e66897d1
PierreMATHIEU <p.mathieu742@gmail.com>
2017-04-20 19:28:02 +0200

    getClothes

--- v1/index.php
+++ v1/index.php
@@ -299,9 +299,7 @@ $app->get('/getClothes', 'authenticate', function(){
                 $tmp1 = array();
                 $tmp1["urlImage"] = $value->getUrlImage();
                 $tmp1["listClothe"] = array();
-                var_dump("OK");
-                var_dump($value);
-                foreach ($value as $value2){
+                foreach ($value->getListClothe as $value2){
                     var_dump("plop");
                     var_dump($value2);
                     $tmp = array();
