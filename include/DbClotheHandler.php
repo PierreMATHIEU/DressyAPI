578ac718dc00e0c784939e28866ad26081dd25c9
PierreMATHIEU <p.mathieu742@gmail.com>
2017-04-24 10:13:53 +0200

    addCloth

--- include/DbClotheHandler.php
+++ include/DbClotheHandler.php
@@ -29,7 +29,7 @@ class DbClotheHandler {
 
 
       $stmt = $this->conn->prepare("INSERT INTO clothe(cloth_brand_id, cloth_category_id, cloth_material_id, cloth_name, cloth_color, cloth_reference, cloth_urlimage, user_id, cloth_created_at) 
-                                              VALUES (:cloth_brand_id, :cloth_category_id, :cloth_material_id, :cloth_name, :cloth_color, :cloth_reference, :cloth_urlimage, :user_id, n$date->getTimestamp())");
+                                              VALUES (:cloth_brand_id, :cloth_category_id, :cloth_material_id, :cloth_name, :cloth_color, :cloth_reference, :cloth_urlimage, :user_id, $date->getTimestamp())");
 
         $stmt->bindValue(':cloth_brand_id',$new_brand[0], PDO::PARAM_INT);
         $stmt->bindValue(':cloth_category_id', $new_cat[0], PDO::PARAM_INT);
