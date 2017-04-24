4b6ec448c5733897b69e99e170dcd6401b88b20a
PierreMATHIEU <p.mathieu742@gmail.com>
2017-04-24 14:57:55 +0200

    addClothes

--- v1/index.php
+++ v1/index.php
@@ -361,9 +361,10 @@ $app->post('/addClothes', 'authenticate', function() use ($app) {
             array_push($clotheArray,$test[0]);
         }
 
-        //var_dump($clotheArray);
+        var_dump("ok");
         $db = new DbClotheHandler();
         $res = $db->createClothes($clothes, $clotheArray);
+        var_dump($res);
 
         if ($res == true ){
             $clothesResponse = new Clothes();
